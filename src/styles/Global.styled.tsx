30cb77d390a6521fbc49b63a15c3fa215cd3f7f6
Yulia Yakusheva <108211145+Yulia-Ya@users.noreply.github.com>
2024-10-26 15:01:27 +0300

    fonts added

---
+++ src/styles/Global.styled.tsx
@@ -0,0 +1,45 @@
+import { createGlobalStyle } from "styled-components";
+import { theme } from "./Theme";
+
+export const GlobalStyles = createGlobalStyle`
+    *,
+    *::before,
+    *::after {
+        margin: 0;
+        padding: 0;
+        box-sizing: border-box;
+
+    }
+
+    @font-face {
+    font-family: 'SF Pro Display';
+    src: url('../assets/fonts/SFProDisplay-Regular.woff2') format('woff2');
+    font-weight: normal;
+    font-style: normal;
+}
+
+@font-face {
+    font-family: 'SF Pro Display';
+    src: url('../assets/fonts/SFProDisplay-Bold.woff2') format('woff2');
+    font-weight: bold;
+    font-style: normal;
+}
+
+@font-face {
+    font-family: 'SF Pro Display';
+    src: url('../assets/fonts/SFProDisplay-Semibold.woff2') format('woff2');
+    font-weight: 600;
+    font-style: normal;
+
+}
+    body {
+    margin: 0;
+    font-family: 'SF Pro Display', sans-serif;
+    -webkit-font-smoothing: antialiased;
+    -moz-osx-font-smoothing: grayscale;
+    color: ${theme.colors.primaryFont}
+}
+
+    
+
+`;
---
+++ src/styles/Theme.tsx
@@ -0,0 +1,9 @@
+export const theme = {
+  colors: {
+    primary: "#1A1A1A",
+    secondary: "#000000",
+    primaryFont: "#FCFCFC",
+    secondaryFont: "#9F9F9F",
+    accent: "#0098EB",
+  },
+};
